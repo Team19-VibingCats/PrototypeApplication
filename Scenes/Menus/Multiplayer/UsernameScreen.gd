708c5d7a4ff137aa2472a7e615f28ddf59c95779
ThomasUijlen <thomasuijlen@hotmail.com>
2022-03-27 23:22:45 +0200

    Username screen stretching

--- Scenes/Menus/Multiplayer/UsernameScreen.gd
+++ Scenes/Menus/Multiplayer/UsernameScreen.gd
@@ -1,14 +1,14 @@
-extends Node2D
+extends Control
 
 func _ready():
-	$LineEdit.text = TokenHandler.username
-	$ColorPickerAnchor/Panel/ColorPicker.color = TokenHandler.color
+	$CenterContainer/VBoxContainer/LineEdit.text = TokenHandler.username
+	$CenterContainer/VBoxContainer/Panel/ColorPicker.color = TokenHandler.color
 
 func _on_Button_pressed():
-	if $LineEdit.text.length() > 1:
-		TokenHandler.username = $LineEdit.text
-		TokenHandler.color = $ColorPickerAnchor/Panel/ColorPicker.color
+	if $CenterContainer/VBoxContainer/LineEdit.text.length() > 1:
+		TokenHandler.username = $CenterContainer/VBoxContainer/LineEdit.text
+		TokenHandler.color = $CenterContainer/VBoxContainer/Panel/ColorPicker.color
 		get_tree().change_scene("res://Scenes/Menus/Multiplayer/WorldScreen.tscn")
 
 func _process(delta):
-	$Mantel.modulate = $ColorPickerAnchor/Panel/ColorPicker.color
+	$CenterContainer/VBoxContainer/Control/Mantel.modulate = $CenterContainer/VBoxContainer/Panel/ColorPicker.color
